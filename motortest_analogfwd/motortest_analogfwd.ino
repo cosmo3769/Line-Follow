bb68ccc345d17936b7214398c5a88c08f5f1eb8c
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-02-14 17:29:08 +0530

    Revert "added motor_both_utube" revert This reverts commit e058ef1acd7931e49a4c92a284b36c5d715456ef.

--- motortest_analogfwd/motortest_analogfwd.ino
+++ motortest_analogfwd/motortest_analogfwd.ino
@@ -1,21 +1,14 @@
 const int motorpin1 = 5;
 const int motorpin2 = 6;
-const int motorpin3 = 9;
-const int motorpin4 = 10;
 
 void setup() {
   pinMode(motorpin1, OUTPUT);
   pinMode(motorpin2, OUTPUT);
-  pinMode(motorpin3, OUTPUT);
-  pinMode(motorpin4, OUTPUT);
 }
 
 void loop() {
 
   analogWrite(motorpin1, 255);
   analogWrite(motorpin2, 0);
-
-  analogWrite(motorpin3, 255);
-  analogWrite(motorpin4, 0);
   
 }
